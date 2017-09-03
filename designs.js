84cb6fd54520425503ad06edae72ea18d8ead81a
Travis Henry <travis.s.henry@gmail.com>
2017-09-03 07:45:03 -0700

    code cleanup, moved scope of rows and cols into function

--- designs.js
+++ designs.js
@@ -6,8 +6,8 @@
 function makeGrid() {
 
     //assign rows and columns to the height and width input (instantiated in higher scope)
-    rows = document.getElementById("input_height").value;
-    cols = document.getElementById("input_width").value;
+    let rows = document.getElementById("input_height").value;
+    let cols = document.getElementById("input_width").value;
 
     //string for displaying the table HTML data
     let tableHTML = "";
@@ -31,14 +31,13 @@ function makeGrid() {
 
 }
 
-
 //submit button and event listener to call the makeGrid function
 let button = document.getElementById("sizePicker").getElementsByTagName("input")[2];
 
 button.addEventListener("click", function(event) {
     event.preventDefault();
     makeGrid();
-})
+});
 
 //currentColor is the form element; currentColorValue is the hex decimal value;
 let currentColor = document.getElementById("colorPicker");
@@ -47,9 +46,5 @@ let currentColorValue = currentColor.value;
 //event listener to constantly update the current color hex decimal value
 currentColor.addEventListener("change", function(event) {
     currentColorValue = this.value;
-})
-
-//instantiate the columns and rows variables so they can be used outside of the makeGrid() function
-let cols, rows = 0;
-
+});
 

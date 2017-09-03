c637568ed780597a840feb667c5273a6cca1db80
Travis Henry <travis.s.henry@gmail.com>
2017-09-02 22:31:29 -0700

    Added variable and event listener for the color picker

--- designs.js
+++ designs.js
@@ -9,8 +9,21 @@ function makeGrid() {
 
 }
 
+//submit button and event listener to call the makeGrid function
 let button = document.getElementById("submitButton");
 
 button.addEventListener("click", function() {
     makeGrid();
 })
+
+//currentColor is the form element; currentColorValue is the hex decimal value;
+let currentColor = document.getElementById("colorPicker");
+let currentColorValue = currentColor.value;
+
+//event listener to constantly update the current color hex decimal value
+currentColor.addEventListener("change", function(event) {
+    currentColorValue = this.value;
+})
+
+
+
